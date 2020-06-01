eeeae86021648ab3440da485f65b4931ae25b13d
Nikita-Belov2020 <66220256+Nikita-Belov2020@users.noreply.github.com>
2020-06-01 17:12:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementByld('myTopnav')
+	if (x.className === "toppnav") {
+		x.className += " responsive";
+		} else{
+			x.className = "topnav";
+		}
+	}
